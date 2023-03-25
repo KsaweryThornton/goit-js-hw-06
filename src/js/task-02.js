818e7b264dc0eb693a00db4fe72da180eeb52797
KsaweryThornton <ksawery.feliks.thornton@gmail.com>
2023-03-25 20:15:37 +0100

    Task-02 done.

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,18 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const list = document.querySelector("#ingredients");
+
+const ingredientsList = ingredients
+  .map((elem) => `<li class="list-item">${elem}</li>`)
+  .join("");
+
+list.innerHTML = ingredientsList;
+
+console.log(list);

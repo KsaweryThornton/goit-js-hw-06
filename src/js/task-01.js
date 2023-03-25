89cb8577e5257b35194148ecbd05e91410e3570c
KsaweryThornton <ksawery.feliks.thornton@gmail.com>
2023-03-25 04:43:10 +0100

    Task-01 done.

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,4 +1,15 @@
 "use strict";
 
-const categories = document.querySelectorAll("#categories .item h2").length;
-const categoriesMessage = `Number of categories: ${categories}`;
+const nOfCategories = document.querySelectorAll("#categories .item h2").length;
+const nOfCategoriesMsg = `Number of categories: ${nOfCategories}`;
+console.log(nOfCategoriesMsg);
+
+const allLists = document.querySelector("#categories");
+const categories = allLists.querySelectorAll(".item");
+
+categories.forEach((elem) => {
+  const categoriesNames = elem.querySelector("h2");
+  console.log(`Category: ${categoriesNames.textContent}`);
+  const categoriesItems = elem.querySelectorAll("li");
+  console.log(`Elements: ${categoriesItems.length}`);
+});

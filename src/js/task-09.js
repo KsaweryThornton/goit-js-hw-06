49d40fa2aaacfeb691fe3110ba8814451f3c106a
KsaweryThornton <ksawery.feliks.thornton@gmail.com>
2023-03-28 21:43:59 +0200

    Task09 - done.

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -3,3 +3,10 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const changeColorBtn = document.querySelector(".change-color");
+const bgColor = document.querySelector(".color");
+
+changeColorBtn.addEventListener("click", (e) => {
+  bgColor.style.backgroundColor = getRandomHexColor();
+});

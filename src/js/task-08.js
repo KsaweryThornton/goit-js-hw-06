be1bbc39371f0a11a5befd4363c0830df9bc613d
KsaweryThornton <ksawery.feliks.thornton@gmail.com>
2023-03-28 21:23:27 +0200

    Task08 - done.

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,16 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", loginFunction);
+
+function loginFunction(e) {
+  e.preventDefault;
+  const {
+    elements: { email, password }
+  } = e.currentTarget;
+  
+  if (email.value === "" || password.value === "") {
+    return alert("Please fill in all the fields!");
+  }
+  console.log(`Email: ${email.value}, Password: ${password.value}`);
+  e.currentTarget.reset();
+}
